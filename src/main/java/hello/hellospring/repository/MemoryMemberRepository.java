13c1c352255c13e56a5a2b9e2255ddcd7dcfb1b9
taesikyoo <yrns0907@gmail.com>
2020-07-23 15:38:17 +0900

    회원 관리 예제 - 웹 MVC 개발

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -11,7 +11,7 @@ public class MemoryMemberRepository implements MemberRepository {
 
     @Override
     public Member save(Member member) {
-        member.setId(seqence++);
+        member.setId(++seqence);
         store.put(member.getId(), member);
         return member;
     }
